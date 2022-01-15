4f05a7772079ca4ddb6cf1410ae51f92dcbeaa81
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2022-01-16 04:17:43 +0530

    sdm660-common : Set TARGET_KERNEL_CLANG_PATH
    credits : kunmun

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,6 +42,8 @@ ifeq ($(BOARD_KERNEL_COMMON),true)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
 endif
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
