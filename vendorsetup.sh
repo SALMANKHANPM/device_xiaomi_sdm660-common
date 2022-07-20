f55fd69fc4081a06089dcc8ecfff49ac8cef6a53
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2022-07-20 16:29:06 +0530

    sdm660-common : clone few repo's

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,12 @@
+#
+# Clone hardware_xiaomi
+git clone https://github.com/PixelExperience/hardware_xiaomi.git hardware/xiaomi
+
 # Clone TinyXML
 git clone https://github.com/SALMANKHANPM/external_tinyxml.git external/tinyxml
 
 # Clone Proton Clang
 git clone --depth=1 https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton
+
+# Clone airbnb-lottie
+git clone https://github.com/SALMANKHANPM/android_external_airbnb-lottie.git -b 12 external/airbnb-lottie
