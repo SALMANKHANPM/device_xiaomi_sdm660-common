99f262ba264a2200aa04a1a62252061e63ebcc65
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2022-07-20 19:41:04 +0530

    sdm660-common : clone proton clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,5 @@
 # Clone TinyXML
 git clone https://github.com/SALMANKHANPM/external_tinyxml.git external/tinyxml
+
+# Clone Proton Clang
+git clone --depth=1 https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton
